e3efe9ed812c092999d246da9c7941792999ff8e
Kevin Yee <kevinyee1993@gmail.com>
2018-03-02 16:30:54 -0800

    completed attributes

--- active_record1/lib/01_sql_object.rb
+++ active_record1/lib/01_sql_object.rb
@@ -6,9 +6,24 @@ require 'active_support/inflector'
 class SQLObject
   def self.columns
     # ...
+    return @columns if @columns
+    cols = DBConnection.execute2(<<-SQL).first
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      LIMIT
+        0
+      SQL
+
+      cols.map!(&:to_sym)
+      @columns = cols
   end
 
   def self.finalize!
+    self.class.columns.each do |col|
+      define_method(col){col}
+    end
   end
 
   def self.table_name=(table_name)
@@ -23,7 +38,7 @@ class SQLObject
 
   def self.all
     # ...
-    
+
   end
 
   def self.parse_all(results)
@@ -40,6 +55,8 @@ class SQLObject
 
   def attributes
     # ...
+    @attributes ||= {}
+
   end
 
   def attribute_values
